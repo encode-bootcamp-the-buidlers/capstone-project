6fc66a039f136766bebf96d1590275dd55c5f208
Tobias Leinss <caruso33@web.de>
2022-08-03 17:21:01 +0200

    Add proposal

--- Blockchain/contracts/GovernanceToken.sol
+++ Blockchain/contracts/GovernanceToken.sol
@@ -10,17 +10,27 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 /// @title Governance token
 /// @notice This token contract creates the governance token for the DAO
 /// @dev ERC20 standard compliant
-contract GovernanceToken is ERC20, ERC20Burnable, Ownable, ERC20Permit, ERC20Votes {
-    constructor() ERC20("Governance Token", "GT") ERC20Permit("Governance Token") {}
+contract GovernanceToken is
+    ERC20,
+    ERC20Burnable,
+    Ownable,
+    ERC20Permit,
+    ERC20Votes
+{
+    constructor(string memory _name, string memory _symbol)
+        ERC20(_name, _symbol)
+        ERC20Permit(_name)
+    {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    function _afterTokenTransfer(address from, address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Votes)
-    {
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
         super._afterTokenTransfer(from, to, amount);
     }
 
