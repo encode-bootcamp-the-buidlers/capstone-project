fac70c23449f67cae9ecfec5cacdefd92d4861c5
vidvidvid <weetopol@gmail.com>
2022-08-06 21:23:39 +0200

    add icons

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
-import { Box, Flex, VStack } from "@chakra-ui/react";
+import { Box, Flex, VStack, Image } from "@chakra-ui/react";
 import { Route, Routes } from "react-router";
 
 import { Home } from "./pages/Home";
 import { MyCollections } from "./pages/MyCollections";
 import { Vote } from "./pages/Vote";
 
+import logo from "./assets/logo.svg";
+import mycollections from "./assets/mycollections.svg";
+import vote from "./assets/vote.svg";
 import "./App.css";
 
 function App() {
@@ -24,7 +27,12 @@ function App() {
           backdropFilter="blur(8px)"
           alignItems="center"
         >
-          <Link to="/">DAOs got talent</Link>
+          <Flex alignItems="center" gap={4}>
+            <Box w={6}>
+              <Image src={logo} alt="logo" />
+            </Box>
+            <Link to="/">DAOs got talent</Link>
+          </Flex>
 
           <Box>Wallet login</Box>
         </Flex>
@@ -32,9 +40,23 @@ function App() {
         {/* MAIN */}
         <Flex w="full" h="full" px={8} pt={8}>
           {/* NAVIGATION */}
-          <Flex w={40} h="full" flexDir="column" gap={6}>
-            <Link to="/vote">Vote</Link>
-            <Link to="/my-collections">My Collections</Link>
+          <Flex w={48} h="full" flexDir="column" gap={6}>
+            <Link to="/vote">
+              <Flex alignItems="center" gap={4}>
+                <Box w={6}>
+                  <Image src={vote} alt="logo" />
+                </Box>
+                <Link to="/">Vote</Link>
+              </Flex>
+            </Link>
+            <Link to="/my-collections">
+              <Flex alignItems="center" gap={4}>
+                <Box w={6}>
+                  <Image src={mycollections} alt="logo" />
+                </Box>
+                <Link to="/">My Collections</Link>
+              </Flex>
+            </Link>
           </Flex>
 
           {/* CONTENT */}
