c73d713cdf024153c00cc9d9e521eedf8e284dec
vidvidvid <weetopol@gmail.com>
2022-08-06 21:09:04 +0200

    set up folder structure and routing

---
+++ Frontend/src/pages/MyCollections.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function MyCollections() {
+  return <h2>MyCollections</h2>;
+}
---
+++ Frontend/src/pages/Vote.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Vote() {
+  return <h2>Vote</h2>;
+}
