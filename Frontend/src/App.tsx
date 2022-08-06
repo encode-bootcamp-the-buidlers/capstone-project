d943eeb7405808a10d1c966eefaa5a5cf463c90f
vidvidvid <weetopol@gmail.com>
2022-08-06 21:09:04 +0200

    set up folder structure and routing

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Box, Flex, VStack } from "@chakra-ui/react";
+import { Route, Routes } from "react-router";
+
+import { Home } from "./pages/Home";
+import { MyCollections } from "./pages/MyCollections";
+import { Vote } from "./pages/Vote";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <VStack h="full">
+        {/* HEADER */}
+        <Flex
+          w="100%"
+          justify="space-between"
+          h={20}
+          zIndex={1000}
+          px={8}
+          background="linear-gradient(rgba(255, 255, 255, 0.1), transparent)"
+          backdropFilter="blur(8px)"
+          alignItems="center"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Link to="/">DAOs got talent</Link>
+
+          <Box>Wallet login</Box>
+        </Flex>
+
+        {/* MAIN */}
+        <Flex w="full" h="full" px={8} pt={8}>
+          {/* NAVIGATION */}
+          <Flex w={40} h="full" flexDir="column" gap={6}>
+            <Link to="/vote">Vote</Link>
+            <Link to="/my-collections">My Collections</Link>
+          </Flex>
+
+          {/* CONTENT */}
+          <Flex w="full" h="full" justifyContent="center">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/vote" element={<Vote />} />
+              <Route path="/my-collections" element={<MyCollections />} />
+            </Routes>
+          </Flex>
+        </Flex>
+      </VStack>
+    </Router>
   );
 }
 
