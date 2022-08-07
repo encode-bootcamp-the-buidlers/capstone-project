074ae067db3d316ba933429e0e3f2fa11fac91b6
sidlami <72751704+sidlami@users.noreply.github.com>
2022-08-07 20:32:48 +0200

    installed axios package; roadmap for getting data from blockchain

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import ethers from "ethers";
 
 export function Home() {
-  return <h2>Home</h2>;
+
+  //on mount inner funtion will be executed
+  useEffect(()=>{
+
+    //connect to Ballot smart contract
+
+    //get collections
+    const getCollections = async () => {
+      //get CID of items of each collection
+
+      //get current amount of votes for each collection
+
+      //get issuer of each collection
+    }
+  }, [])
+  return(
+    <div>
+      <img src="https://ipfs.infura.io/ipfs/QmRoLZm3kyDNjK7joNQepwVm56URE9kWSWXYzckNjXPvGx"></img>
+    </div>
+  );
 }
