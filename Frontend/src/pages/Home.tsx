67b1f4c28b4f0c24cf81213afcb5ec3d3f9d4182
sidlami <72751704+sidlami@users.noreply.github.com>
2022-08-08 13:12:49 +0200

    demo of collection in frontend

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -2,6 +2,30 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 import ethers from "ethers";
 
+//hardcoded collection data
+const collections = [
+  {
+    name : 1,
+    items : [
+      "QmRoLZm3kyDNjK7joNQepwVm56URE9kWSWXYzckNjXPvGx",
+      "Qmcj35PdCz5FoZSLwFZKacezJmGQdMBPjoNxBeovFuGrk1",
+      "QmV6TRU7T1w3urmeFvry7S6XfEExiRCGGvsrjZEfxsKopT",
+      "QmRoLZm3kyDNjK7joNQepwVm56URE9kWSWXYzckNjXPvGx",
+      "QmfVGDguVa3VBeSCJjwVDjyoh5EwD7i6CvbB4UHHRBo1Pd"
+    ]
+  },
+  {
+    name : 2,
+    items : [
+      "QmSarasRVaRZHE49xCvwzN4cjG4m18TL3JD4Yeto4ieXhc",
+      "QmfJuzD5ABPrLVwN1eFAtZEaHByAJ6pxWtBgckCfbu1iqf",
+      "QmSKaPUS61nMiB5jySDUEDxCPpt1DSK1KNUgnxmHi2Xj9G",
+      "QmX434XD4eRWXUjM2Ctdu1wMDAdA3s1d8AzpY6Eh9unCjX",
+      "QmQ5LSpzV8bcnhGAgg7c4YYNPjsjSWZgcHwoYZVgqegR8U",
+      "QmS5gxySjx8q32E7pG47S17aCyGuLGbMa3PNqUw83CRy9u"
+    ]
+  },
+]
 export function Home() {
 
   //on mount inner funtion will be executed
@@ -18,9 +42,28 @@ export function Home() {
       //get issuer of each collection
     }
   }, [])
+
+  //rende component
   return(
     <div>
-      <img src="https://ipfs.infura.io/ipfs/QmRoLZm3kyDNjK7joNQepwVm56URE9kWSWXYzckNjXPvGx"></img>
+      {
+        collections ? 
+          collections.map(collection => {
+            return(
+              <div>
+                <h4>{collection.name}</h4>
+                {
+                  collection.items.map(item => (
+                    <img src={"https://ipfs.infura.io/ipfs/"+item}></img>
+                  ))
+                }
+                
+              </div>
+            )
+          })
+        :
+        ""
+      }
     </div>
   );
 }
