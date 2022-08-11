dc67b5cf9e38ca3dcdebb57824d586c3bd93944d
Bitcoinera <anagjordano@gmail.com>
2022-08-11 16:11:41 +0400

    fixing my collections

--- Frontend/src/pages/MyCollections.tsx
+++ Frontend/src/pages/MyCollections.tsx
@@ -1,10 +1,20 @@
-import React, { useEffect } from "react";
-
+import React, { useEffect, useState } from "react";
 import { Gallery } from "../components/Gallery";
 import { ContentWrapper } from "../components/PageWrapper";
-import { collections } from "../utils/collections";
 
-export function MyCollections() {
+import { ethers } from "ethers";
+
+const axios = require("axios");
+
+interface Props {
+  daoContract: ethers.Contract;
+}
+
+export function MyCollections(props: Props) {
+  const [collections, setCollections] = useState<any[]>([]);
+  const [percents, setPercents] = useState<any[]>([]);
+  const [galleries, setGalleries] = useState<any[]>([]);
+
   useEffect(() => {
     //connect to Ballot smart contract
 
@@ -13,23 +23,37 @@ export function MyCollections() {
       //get CID of items of each collection
       //get current amount of votes for each collection
       //get issuer of each collection
+      try {
+        const collections = await props.daoContract.getAccountCollections();
+        setCollections(collections);
+        console.log("COLLECTIONS", collections);
+      } catch (error) {
+        console.log(error);
+      }
     };
+    const getPercentages = async () => {
+      // get percentages of each collection
+      try {
+        const percents = await props.daoContract.getVotePercentages();
+        setPercents(percents.map((percent: any) => percent / 100));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getCollections();
+    getPercentages();
   }, []);
   return (
     <ContentWrapper>
       <Gallery
-        images={collections[0].items.map(
-          (item) => "https://ipfs.io/ipfs/" + item
-        )}
+        images={collections.map((item: any) => "https://ipfs.io/ipfs/" + item)}
         artistName="T"
-        percent={67}
+        percent={percents[0]}
       />
       <Gallery
-        images={collections[1].items.map(
-          (item) => "https://ipfs.io/ipfs/" + item
-        )}
+        images={collections.map((item: any) => "https://ipfs.io/ipfs/" + item)}
         artistName="K"
-        percent={43}
+        percent={percents[1]}
       />
     </ContentWrapper>
   );
